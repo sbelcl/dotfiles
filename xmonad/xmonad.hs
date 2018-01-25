d42c9fbc53e6d34d7332cf1d41668e5a893b4e33
Imnos <sbelcl@gmail.com>
2018-01-25 13:05:18 +0100

    Posodbljene skoraj vse datoteke

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -12,37 +12,46 @@ import XMonad.Actions.CycleWS
 import XMonad.Hooks.ManageDocks
 --import XMonad.Hooks.Urgencyhook
 --import XMonad.Util.NamedWindows
---import XMonad.Util.Run
-
+import XMonad.Util.SpawnOnce
+import XMonad.Config.Kde
 
 myLayoutHook            = onWorkspace "1:term" (tall ||| Mirror tall ||| Full) 
                         $ onWorkspace "5:wine" Full          
                         $ (Full ||| tall ||| Mirror tall) 
                                 where 
                                     tall = Tall 1 0.02 0.7 
-myManageHook = composeAll 
-    [ className =? "Chromium" --> doShift "2:www"
-    , className =? "URxvt" --> doShift "1:term"
-    , className =? "Vlc" --> doFloat
-    , className =? "Firefox" --> doShift "2:www"
-    , className =? "Thunderbird" --> doShift "4:e-mail"
-    , className =? "Xmessage" --> doFloat
-    , className =? "Wine" --> doShift "5:wine"
-    , className =? "Qbittorrent" --> doFloat
-    , className =? "Gimp" --> doFloat 
-    , className =? "Wpa_gui" --> doFloat
-    , className =? "Tilda" --> doFloat
-    , className =? "conky" --> doIgnore
-    , className =? "MacSlow Cairo Ura" --> doFloat
+myManageHook = composeAll . concat $
+    [ [ className =? c --> doFloat                    | c <- myFloats ]
+    , [ className =? c --> doIgnore                   | c <- myIgnores ]
+    , [ className =? c --> doF (W.shift "1:term")     | c <- myTerminals ]
+    , [ className =? c --> doF (W.shift "2:www")      | c <- myWebBrowsers ]
+    , [ className =? c --> doF (W.shift "3:media")    | c <- myMediaEditors ]
+    , [ className =? c --> doF (W.shift "4:PIM")      | c <- myPIMManagers ]
+    , [ className =? c --> doF (W.shift "5:games")    | c <- myGames ]
+    , [ className =? c --> doF (W.shift "9:torrent")  | c <- myTorrents ]
     ]
+    where 
+        myFloats        = ["Vlc", "Gimp", "Prompt", "Overjanje", "ark", "Wine", "ksysguard", "Yakuake"]
+        myTerminals     = ["URxvt", "Konsole"]
+        myWebBrowsers   = ["Chromium","Firefox"]
+        myMediaEditors  = ["Gimp", "Inkscape"] 
+        myPIMManagers   = ["Thunderbird"] 
+        myGames         = ["Wine", "Steam"] 
+        myTorrents      = ["qBittorrent"] 
+        myIgnores       = ["Yakuake"] 
+
+
+
 -- myLogHook               = return ()
 
 myStartupHook           = do
-    addScreenCorners [ (SCLowerRight, nextWS)
-                      ,(SCLowerLeft,  prevWS)
-		      ,(SCUpperRight, spawn "cairo-clock") 	
-                     ]
     setDefaultCursor xC_left_ptr
+    spawnOnce "~/bin/clearScreen.sh"
+    spawnOnce "xcompmgr -f"
+    spawnOnce "urxvt"
+    spawnOnce "firefox"
+    spawnOnce "feh --randomize --bg-fill ~/ozadja/"
+    
 
 	
 myEventHook e 		= do
@@ -60,7 +69,9 @@ myEventHook e 		= do
 myKeys = [ ("M-m", spawn "date +'%c' |  dzen2 -p 4")
          , ("M-s", spawn "conky -c /home/imnos/.conky/Gotham/Gotham |  dzen2 -p 4")
          , ("M-e", spawn "urxvt -e ranger")
+         , ("M-<Escape>", spawn "ksysguard")
 	 , ("M-d", date)	
+	 , ("M-<Print>", spawn "scrot -q 1 $HOME/slike/screenshots/%Y-%m-%d-%H:%M:%S.png > /home/imnos/errors.log 2>&1")
 	 , ("<XF86AudioMute>", spawn "~/.xmonad/bin/muteToggle")
 	 , ("<XF86AudioLowerVolume>", spawn "~/.xmonad/bin/volDown")
 	 , ("<XF86AudioRaiseVolume>", spawn "~/.xmonad/bin/volUp")
@@ -75,10 +86,10 @@ main =  xmonad $ defaultConfig
         ,borderWidth         = 0
         ,focusFollowsMouse   = True
         ,modMask             = mod4Mask
-        ,workspaces          = ["1:term", "2:www", "3:media", "4:e-mail", "5:wine"] ++ map show [6..8] ++ ["9:torrent"]
+        ,workspaces          = ["1:term", "2:www", "3:media", "4:PIM", "5:wine"] ++ map show [6..8] ++ ["9:torrent"]
 --    ,mouseBindings       = myMouseBindings
         ,layoutHook          = myLayoutHook
-        ,manageHook          = myManageHook
+        ,manageHook          = manageHook kdeConfig <+> myManageHook
         ,handleEventHook     = myEventHook <+> docksEventHook
 --    ,logHook             = myLogHook
 --    ,startupHook         = setDefaultCursor xC_left_ptr
